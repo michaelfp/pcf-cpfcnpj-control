1ba56963661a5d95825702f32109cd7c1530da27
Michael Ferro Pereira <mpereira@alfapeople.com.br>
2019-08-18 23:21:59 -0300

    Adicionado Projeto

---
+++ CPFCNPJ/CPFCNPJValidator.ts
@@ -0,0 +1,125 @@
+import { number } from "prop-types";
+
+
+export enum CPFCNPJType
+{
+    CPF = 1,
+        CNPJ = 0
+}
+
+export class CPFCNPJValidator{
+    
+    _regexNondigit: RegExp = /\D/g;
+    
+
+
+    
+    _isRepeatingChar(data:string): boolean{
+
+        return data.split('').every((currentChar) => { currentChar === data[0]});
+    }
+
+    public validaCPF(_cpf:string):boolean{
+
+        let cpfNumerico = _cpf.replace(this._regexNondigit,'');
+        if(!this._isRepeatingChar(cpfNumerico))
+        {
+            const mod11 = ( num:number ) => num % 11;
+
+            const toSumOfProducts = ( multiplier:any ) => ( result:any, num:any, i:any ) =>  result + ( num * multiplier-- );
+
+            const getSumOfProducts = ( list:string[], multiplier:number ) => list.reduce( toSumOfProducts(multiplier),0);
+
+            const getValidationDigit = ( multiplier:any ) => ( cpf:string[] ) => getDigit(mod11(getSumOfProducts(cpf, multiplier ) ) );
+
+            const getDigit = ( num:any ) =>   ( num > 1 )  ? 11 - num  : 0;
+
+            let CPF = cpfNumerico.slice(0,9).split('');
+            const firstDigit = getValidationDigit(10)(CPF).toString();
+            const secondDigit = getValidationDigit(11)(CPF.concat(firstDigit)).toString();
+
+            CPF = CPF.concat(firstDigit);
+            CPF = CPF.concat(secondDigit);
+
+            return cpfNumerico === CPF.join('');
+            
+            console.log("validaCPF");
+        }
+                
+        return false;
+    }
+
+    public isCPF(cpf:string):boolean{
+        let cpfNumerico = cpf.replace(this._regexNondigit,'');
+
+        if(cpfNumerico.length == 11)
+            return true;
+
+        return false;
+    }
+
+    public formataCPF(cpf:string):string{
+        console.log("FormataCPF");
+        let cpfNumerico = cpf.replace(this._regexNondigit,'');
+        let cpfFormatado = cpfNumerico.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,'$1.$2.$3-$4');
+        
+        return cpfFormatado;
+    }
+
+    public validaCNPJ(cnpj:string):boolean{
+        console.log("validaCNPJ");
+
+        let cnpjNumerico = cnpj.replace(this._regexNondigit,'');
+        if(!this._isRepeatingChar(cnpjNumerico))
+        {
+            // Cáculo de validação
+	        let t = cnpjNumerico.length - 2,
+                d = cnpjNumerico.substring(t),
+                d1 = parseInt(d.charAt(0)),
+                d2 = parseInt(d.charAt(1)),
+                calc = (x:any) => {
+                    let n = cnpjNumerico.substring(0, x),
+                        y = x - 7,
+                        s = 0,
+                        r = 0
+
+                        for (let i = x; i >= 1; i--) {
+                            s += parseInt(n.charAt(x - i)) * y--;
+                            if (y < 2)
+                                y = 9
+                        }
+
+                        r = 11 - s % 11
+                        return r > 9 ? 0 : r
+                }
+
+            debugger;
+            let firstDigit = calc(t).toString();
+            let secondDigt = calc(t+1).toString();
+            let CNPJDigitos = '';
+            CNPJDigitos = CNPJDigitos.concat(firstDigit);
+            CNPJDigitos = CNPJDigitos.concat(secondDigt);
+            
+            return cnpjNumerico.substring(12) === CNPJDigitos;
+            
+        }
+        return false;
+    }
+
+    public isCNPJ(cnpj:string):boolean{
+        let cnpjNumerico = cnpj.replace(this._regexNondigit,'');
+
+        if(cnpjNumerico.length == 14)
+            return true;
+
+        return false;
+    }
+
+    public formataCNPJ(cnpj:string):string{
+        let cnpjNumerico = cnpj.replace(this._regexNondigit,'');
+        //88.044.098/0001-20
+        let cnpjFormatado = cnpjNumerico.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,'$1.$2.$3/$4.$5');
+        
+        return cnpjFormatado;
+    }
+}
---
+++ CPFCNPJ/index.ts
@@ -0,0 +1,192 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { string } from "prop-types";
+import {CPFCNPJValidator, CPFCNPJType} from './CPFCNPJValidator';
+
+
+
+export class CPFCNPJ implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	_inputControl: HTMLInputElement;
+	_notifyOutputChanged: () => void;
+	_container: HTMLDivElement;
+	_spanControl: HTMLSpanElement;
+	_iconControl: HTMLElement;
+	_context: ComponentFramework.Context<IInputs>;
+	_campoFormatado: boolean;
+	_type:CPFCNPJType;
+	_CPF_CNPJ: string;
+
+	_refreshData: EventListenerOrEventListenerObject;
+
+	_validador = new CPFCNPJValidator();
+
+	/**
+	 * Empty constructor.
+	 */
+	constructor()
+	{
+
+	}
+
+	renderControl(): void{
+
+		this._inputControl = document.createElement("input");
+		this._inputControl.setAttribute("id","cpfcnpj_control");
+		this._inputControl.setAttribute("value",this._context.parameters.CFPCNPJValue.formatted ? this._context.parameters.CFPCNPJValue.formatted : "");
+		this._inputControl.addEventListener("input",this._refreshData);
+		this._spanControl = document.createElement("span");
+		this._iconControl = document.createElement("i");
+		this._iconControl.classList.add("fas");
+		
+		this._spanControl.appendChild(this._iconControl);
+		this._container.appendChild(this._inputControl);
+		this._container.appendChild(this._spanControl);
+
+	}
+
+	refreshData(evt: Event):void{
+		this._CPF_CNPJ = this._inputControl.value;
+
+		
+		if(this._CPF_CNPJ === "" || this._CPF_CNPJ === undefined)
+		{
+			this._iconControl.classList.remove("control-valid");
+			this._iconControl.classList.remove("fa-check-circle");
+			this._iconControl.classList.remove("fa-times-circle");
+			this._iconControl.classList.remove("control-invalid");
+			
+		}
+		else
+			this.validateField();
+		
+		
+		this._notifyOutputChanged();
+	}
+	
+	validateField():void
+	{
+		if(this._type === CPFCNPJType.CPF && (this._CPF_CNPJ !== "" && this._CPF_CNPJ !== undefined))
+		{
+			if(this._validador.isCPF(this._CPF_CNPJ)  && this._validador.validaCPF(this._CPF_CNPJ))
+			{
+				console.log('CPF Válido');
+				this._iconControl.classList.remove("control-invalid");
+				this._iconControl.classList.remove("fa-times-circle");
+				this._iconControl.classList.add("fa-check-circle");
+
+				this._iconControl.classList.add("control-valid");
+				if(this._campoFormatado)
+				{
+					this._CPF_CNPJ = this._validador.formataCPF(this._CPF_CNPJ);
+					this._inputControl.setAttribute("value",this._CPF_CNPJ);
+				}
+
+				this._inputControl.setAttribute("value",this._CPF_CNPJ);
+			}
+			else
+			{
+				this._CPF_CNPJ = "";
+				console.log('CPF Inválido');
+				this._iconControl.classList.remove("control-valid");
+				this._iconControl.classList.remove("fa-check-circle");
+				this._iconControl.classList.add("fa-times-circle");
+				this._iconControl.classList.add("control-invalid");
+			}
+		}
+		
+
+		if(this._type === CPFCNPJType.CNPJ && (this._CPF_CNPJ !== "" && this._CPF_CNPJ !== undefined))
+		{
+			if(this._validador.isCNPJ(this._CPF_CNPJ)  && this._validador.validaCNPJ(this._CPF_CNPJ))
+			{
+				console.log('CNPJ Válido');
+				this._iconControl.classList.remove("control-invalid");
+				this._iconControl.classList.remove("fa-times-circle");
+				this._iconControl.classList.add("fa-check-circle");
+				this._iconControl.classList.add("control-valid");
+				if(this._campoFormatado)
+				{
+					this._CPF_CNPJ = this._validador.formataCNPJ(this._CPF_CNPJ);
+					this._inputControl.setAttribute("value",this._CPF_CNPJ);
+				}
+			}
+			else{
+				this._CPF_CNPJ = "";
+				console.log('CNPJ Inválido');
+				this._iconControl.classList.remove("control-valid");
+				this._iconControl.classList.remove("fa-check-circle");
+				this._iconControl.classList.add("fa-times-circle");
+				this._iconControl.classList.add("control-invalid");
+			}
+
+		}
+		
+	}
+
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='starndard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+	{
+		// Add control initialization code
+
+		this._container = container;
+		this._container = document.createElement("div");
+		this._context = context;
+		this._refreshData = this.refreshData.bind(this);
+		this._notifyOutputChanged = notifyOutputChanged;
+		this._type = context.parameters.TipoControle.raw ? CPFCNPJType.CPF : CPFCNPJType.CNPJ;
+		this._campoFormatado = context.parameters.Formatado.raw;
+		console.log(context.parameters.Formatado.raw);
+		console.log(context.parameters.TipoControle.raw);
+		// console.log(this._type);
+		container.appendChild(this._container);
+		this.renderControl();
+		this.validateField();
+
+	}
+
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView(context: ComponentFramework.Context<IInputs>): void
+	{
+		// Add code to update control view
+		this._CPF_CNPJ = context.parameters.CFPCNPJValue.raw;
+		this._type = context.parameters.TipoControle.raw ? CPFCNPJType.CPF : CPFCNPJType.CNPJ;
+		this._campoFormatado = context.parameters.Formatado.raw;
+		this._context = context;
+		this._inputControl.setAttribute("value",context.parameters.CFPCNPJValue.formatted ? context.parameters.CFPCNPJValue.formatted : "");
+		this.validateField();
+		
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs
+	{
+		return {
+			CFPCNPJValue: this._CPF_CNPJ
+		};
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void
+	{
+		// Add code to cleanup control if necessary
+		this._inputControl.removeEventListener("input",this._refreshData);
+	}
+}
